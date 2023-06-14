0efab41a0915a334c0251b02b1b394fdb33cecdf
wade <1171835133@qq.com>
2023-06-14 20:21:59 +0800

    feat:增加log日志记录帮助类

--- common/log_helper.py
+++ common/log_helper.py
@@ -4,17 +4,44 @@
 # @Author  : wade
 
 import logging
-import os
+import functools
+import threading
+from common.settings import settings
 
-import yaml
+def singleton(cls):
+    cls.__new_original__ = cls.__new__
 
-with open('logging.conf') as f:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    logging.f(yaml.safe_load(f.read()))
+    @functools.wraps(cls.__new__)
+    def singleton_new(cls, *args, **kwargs):
+        # 同步锁
+        with threading.Lock():
+            it = cls.__dict__.get('__it__')
+            if it is not None:
+                return it
+
+            cls.__it__ = it = cls.__new_original__(cls, *args, **kwargs)
+            it.__init_original__(*args, **kwargs)
+            return it
+
+    cls.__new__ = singleton_new
+    cls.__init_original__ = cls.__init__
+    cls.__init__ = object.__init__
+    return cls
 
 
-def get_logging(name=None):
-    if name:
-        return logging.getLogger(name)
-    return logging
+
+
+
+@singleton
+class LogHelper:
+    logger = logging.getLogger("simple")
+
+    def __init__(self):
+        logging.config.fileConfig(settings.log_conf)
+
+    @classmethod
+    def Logger(cls, logger_name) -> logging.Logger:
+        return logging.getLogger(logger_name)
+
+
+log = LogHelper().Logger('simple')
